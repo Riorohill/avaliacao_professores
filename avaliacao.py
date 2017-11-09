Average bug fix.

avaliacao.py
@@ -1,20 +1,26 @@
-from optparse import OptionParser
-from collections import defaultdict
 import csv
 import os
 import matplotlib
 import numpy as np
-from pylatex import Document, NoEscape, Section, Subsection, Command, Figure, Head, MiniPage, PageStyle, Foot,  LargeText, \
-    MediumText, LineBreak, simple_page_number, StandAloneGraphic, Package
+
+from optparse import OptionParser
+from collections import defaultdict
+from pylatex import Document, NoEscape, Section, Subsection, Figure, Head, MiniPage, PageStyle, Foot,  LargeText, \
+    MediumText, LineBreak, StandAloneGraphic, Package
 from pylatex.utils import bold
-import pylatex.config as cf
+
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
 def plot_graph(avals_t,int_i,int_s):
+    '''Updates graph for LATEX inclusion'''
     q_i = []
     acum = 0
+    answers_n = 0
     for i in range(int_i,int_s):
-        acum += sum(list(map(int,list(filter(None, avals_t[i])))))
+        info = list(map(int,list(filter(None, avals_t[i]))))
+        acum += sum(info)
+        answers_n += len(info)
         q_i.append([avals_t[i].count('1'),avals_t[i].count('2'),avals_t[i].count('3'),avals_t[i].count('4'),avals_t[i].count('5')])
     q_i_t = [list(i) for i in zip(*q_i)]
     ind = np.arange(int_s - int_i)
@@ -26,7 +32,7 @@ def plot_graph(avals_t,int_i,int_s):
     rects4 = ax.bar(ind + 3*width, tuple(q_i_t[3]), width, color='b')
     rects5 = ax.bar(ind + 4*width, tuple(q_i_t[4]), width, color='m')
     ax.set_ylabel('Avaliações')
-    ax.set_title('Conceito e Número de Avaliações. Média {:04.2f}.'.format(acum/((int_s - int_i)*5)))
+    ax.set_title('Conceito e Número de Avaliações. Média {:04.2f}.'.format(acum/answers_n))
     ax.set_xticks(ind + width / (int_s - int_i))
     question_label = []
     for i in range(int_i,int_s):
@@ -35,15 +41,20 @@ def plot_graph(avals_t,int_i,int_s):
     ax.legend((rects1[0], rects2[0], rects3[0], rects4[0], rects5[0]), ('Péssimo', 'Ruim', 'Regular', 'Bom', 'Excelente'))
 
 def generate_pdf(n_alunos, n_respostas, avals, cod, prof):
+    '''Creates the body of the PDF file'''
+
     avals_t = [list(i) for i in zip(*avals)]
+
     intro = """
     O Centro Acadêmico de Engenharia de Controle e Automação (CAECA) realizou uma pesquisa com os graduandos do curso sobre o desempenho dos professores do departamento nas disciplinas lecionadas. Com o principal objetivo de fornecer um feedback tanto para o departamento como para os professores, para que se possa melhorar o ensino   -aprendizagem do curso.
 
-    A pesquisa foi realizada durante os dias 01/06 a 01/07 e poderia ser respondida online ou através de formulários impressos entregues aos graduandos. Os resultados da pesquisa não serão divulgados e apenas membros do centro acadêmico e os endereçados terão conhecimento dos mesmos. Por fim, somente matérias que obtiveram mais de 25\% de respostas (em relação ao total de alunos matriculados na disciplina) tiveram seus dados levantados e enviados aos respectivos professores.
+    A pesquisa foi realizada durante os dias 13/11 a 01/12 e poderia ser respondida online ou através de formulários impressos entregues aos graduandos. Os resultados da pesquisa não serão divulgados e apenas membros do centro acadêmico e os endereçados terão conhecimento dos mesmos. Por fim, somente matérias que obtiveram mais de 25\% de respostas (em relação ao total de alunos matriculados na disciplina) tiveram seus dados levantados e enviados aos respectivos professores.
     """
+
     resultados = """
     O formulário de pesquisa consiste em 15 perguntas divididas em 5 categorias: Perfil do Professor, Plano de Ensino, Metodologia de Ensino, Auto Avaliação e Crítica/Sugestões. Das 14 perguntas, 13 perguntas são quantitativas de 1 a 5, simbolizando respectivamente péssimo, ruim, regular, bom e excelente e 1 pergunta aberta.
     """
+
     matriculas = list(filter(None, avals_t[0]))
     participacao = """
     Porcentagem de alunos matriculados na disciplina que responderam à pesquisa: {}\% ({}/{}).
@@ -52,6 +63,7 @@ def generate_pdf(n_alunos, n_respostas, avals, cod, prof):
 
     Total de alunos que não informaram a matrícula: {}.
     """.format(100*int(n_respostas)/int(n_alunos),n_respostas,n_alunos,', '.join(matriculas), len(avals_t[0])-len(matriculas))
+
     perfil = """
     Perguntas quantitativas:
 
@@ -61,6 +73,7 @@ def generate_pdf(n_alunos, n_respostas, avals, cod, prof):
 
     Q3. O professor apresenta domínio do assunto ministrado?
     """
+
     plano = """
     Perguntas quantitativas:
 
@@ -70,6 +83,7 @@ def generate_pdf(n_alunos, n_respostas, avals, cod, prof):
 
     Q6. São apresentados materiais/bibliografias sobre o conteúdo estudado?
     """
+
     metodologia = """
     Perguntas quantitativas:
 
@@ -83,6 +97,7 @@ def generate_pdf(n_alunos, n_respostas, avals, cod, prof):
 
     Q11. O método de avaliação é condizente com a disciplina?
     """
+
     auto = """
     Perguntas quantitativas :
 
@@ -90,33 +105,14 @@ def generate_pdf(n_alunos, n_respostas, avals, cod, prof):
 
     Q13. Sinto que consegui aprender o conteúdo que foi ensinado?
     """
+
     comments = list(filter(None, avals_t[14]))
     if len(comments) == 0 : comments.append("Nenhum comentário registrado.")
     sugestoes = """
     Foram recebidos os seguintes comentários:
 
     """
-    '''
-    q_i = []
-    for i in range(1,4):
-        q_i.append([avals_t[i].count('1'),avals_t[i].count('2'),avals_t[i].count('3'),avals_t[i].count('4'),avals_t[i].count('5')])
-    q_i_t = [list(i) for i in zip(*q_i)]
-    N = 3
-    ind = np.arange(N)
-    width = 0.12
-    fig, ax = plt.subplots()
-    rects1 = ax.bar(ind, tuple(q_i_t[0]), width, color='r')
-    rects2 = ax.bar(ind + width, tuple(q_i_t[1]), width, color='y')
-    rects3 = ax.bar(ind + 2*width, tuple(q_i_t[2]), width, color='g')
-    rects4 = ax.bar(ind + 3*width, tuple(q_i_t[3]), width, color='b')
-    rects5 = ax.bar(ind + 4*width, tuple(q_i_t[4]), width, color='m')
 
-    ax.set_ylabel('Avaliações')
-    ax.set_title('Conceito e Número de Avaliações')
-    ax.set_xticks(ind + width / 3)
-    ax.set_xticklabels(('Q1', 'Q2', 'Q3'))
-    ax.legend((rects1[0], rects2[0], rects3[0], rects4[0], rects5[0]), ('Péssimo', 'Ruim', 'Regular', 'Bom', 'Excelente'))
-    '''
     geometry_options = {
         "head": "40pt",
         "margin": "0.5in",
@@ -131,7 +127,6 @@ def generate_pdf(n_alunos, n_respostas, avals, cod, prof):
         header.append(StandAloneGraphic(image_options="width=360px", filename=logo_file))
     with header.create(Foot("C")):
         header.append("Universidade Federal de Santa Catarina – Conselho de Entidades do Centro Tecnológico – Campus Universitário, S/N - CEP 88045-108 – Florianópolis/SC –  (48) 3721-7698")
-
     doc.preamble.append(header)
     doc.change_document_style("header")
     with doc.create(MiniPage(align='c',width=r"0.9\textwidth")):
@@ -151,7 +146,6 @@ def generate_pdf(n_alunos, n_respostas, avals, cod, prof):
             plot_graph(avals_t,1,4)
             with doc.create(Figure(position='H')) as plot:
                 plot.add_plot(width=NoEscape(r'0.5\textwidth'))
-                #plot.add_caption('')
         with doc.create(Subsection('Plano de Ensino')):
             doc.append(NoEscape(plano))
             plot_graph(avals_t,4,7)
@@ -173,6 +167,7 @@ def generate_pdf(n_alunos, n_respostas, avals, cod, prof):
                 doc.append(NoEscape('"{}"'.format(comentario)))
                 doc.append(NoEscape('\n'))
     doc.generate_pdf('Avaliacao_{}_{}'.format(cod,prof), clean_tex=False)
+
 def main():
     usage = """%prog arquivos
     Os arquivos devem ser .csv codificados como UTF-8 e separados por ','.